3602ffb7f271e1b3a869f4e1524981b52fa6aae0
AbhijithKandala <abhijithkandala7@gmail.com>
2026-09-26 19:55:00 +0530

    feat(store):  Guard map access with RWMutex

--- internal/store/mutex_store.go
+++ internal/store/mutex_store.go
@@ -1,19 +1,28 @@
 package store
 
+import (
+	"sync"
+)
+
 type MutexStore struct {
+	mu   sync.RWMutex
 	data map[string]string
 }
 
 func NewMutexStore() *MutexStore {
-	return &MutexStore{make(map[string]string)}
+	return &MutexStore{data: make(map[string]string)}
 }
 
 func (s *MutexStore) Set(key, value string) {
+	s.mu.Lock()
 	s.data[key] = value
+	s.mu.Unlock()
 	return
 }
 
 func (s *MutexStore) Get(key string) (string, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	val, isPresent := s.data[key]
 	if !isPresent {
 		return "", ErrEntryNotFound
@@ -21,6 +30,8 @@ func (s *MutexStore) Get(key string) (string, error) {
 	return val, nil
 }
 func (s *MutexStore) Del(key string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	_, isPresent := s.data[key]
 	if !isPresent {
 		return ErrEntryNotFound
